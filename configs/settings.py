b5ac8b84684664bb01bd158d9b37c8ddeeb7faa9
Martozhyna <mybonanzaluck@gmail.com>
2023-04-13 22:39:59 +0300

    lesson 6 token blacklist add

--- configs/settings.py
+++ configs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 
     #     my_apps
     'core',
